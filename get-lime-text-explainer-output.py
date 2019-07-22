cca23c96fd00095e237e8ae0123605fd087d3df2
Neal Grantham <neal@nsgrantham.com>
2019-07-21 18:36:40 -0700

    Include class name in local exp data and color by class

--- get-lime-text-explainer-output.py
+++ get-lime-text-explainer-output.py
@@ -48,7 +48,7 @@ text_data = exp.domain_mapper.indexed_string.raw_string()
 with open("data/text-data.txt", "w") as f:
     f.write(text_data)
 
-local_exp = [{"textWord": text_word, "contribProba": contrib_proba} for text_word, contrib_proba in exp.domain_mapper.map_exp_ids(exp.local_exp[1])]
+local_exp = [{"textWord": text_word, "contribProba": abs(contrib_proba), "className": class_names[int(contrib_proba > 0)]} for text_word, contrib_proba in exp.domain_mapper.map_exp_ids(exp.local_exp[1])]
 with open("data/local-exp.json", "w") as f:
     json.dump(local_exp, f)
 
--- js/bars.js
+++ js/bars.js
@@ -55,3 +55,43 @@ d3.json("data/predict-proba.json", function(data) {
   svg1.append("g")
       .call(d3.axisLeft(y));
 })
+
+// append the svg object to the body of the page
+// append a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+var svg2 = d3.select("#area2").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", 
+          "translate(" + margin.left + "," + margin.top + ")");
+
+d3.json("data/local-exp.json", function(data) {
+  // format the data
+  data.forEach(function(d) {
+    d.contribProba = +d.contribProba;
+  });
+
+  // Scale the range of the data in the domains
+  x.domain([0, d3.max(data, function(d){ return d.contribProba; })])
+  y.domain(data.map(function(d) { return d.textWord; }));
+
+  // append the rectangles for the bar chart
+  svg2.selectAll(".bar")
+      .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("width", function(d) {return x(d.contribProba); } )
+      .attr("y", function(d) { return y(d.textWord); })
+      .attr("height", y.bandwidth())
+      .style("fill", function(d) { return color(d.className)});
+
+  // add the x Axis
+  svg2.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  // add the y Axis
+  svg2.append("g")
+      .call(d3.axisLeft(y));
+})
